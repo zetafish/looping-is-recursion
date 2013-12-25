bc53f55d09dcdd926618ca85ee988304632c8499
Endymion Kasanardjo <zetafish@gmail.com>
2013-12-26 00:24:23 +0100

    solved

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -2,11 +2,10 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  (let [aux (fn [acc n]
-              (if (zero? n)
-                acc
-                (recur (* acc base) (dec n))))]
-    (aux 1 exp)))
+  (loop [acc 1 n exp]
+    (if (zero? n)
+      acc
+      (recur (* acc base) (dec n)))))
 
 (defn last-element [a-seq]
   (cond (empty? a-seq) nil
@@ -20,34 +19,33 @@
         :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
-  (let [aux (fn [n s]
-              (cond (empty? s) nil
-                    (pred (first s)) n
-                    :else (recur (inc n) (rest s))))]
-    (aux 0 a-seq)))
+  (loop [n 0
+         s a-seq]
+    (cond (empty? s) nil
+          (pred (first s)) n
+          :else (recur (inc n) (rest s)))))
 
 (defn avg [a-seq]
-  (let [aux (fn [sum count s]
-              (if (empty? s)
-                (/ sum count)
-                (recur (+ (first s) s) (inc count) (rest s))))]
-    (aux 0 0 a-seq)))
+  (loop [sum 0
+         count 0
+         s a-seq]
+    (if (empty? s)
+      (/ sum count)
+      (recur (+ (first s) sum) (inc count) (rest s)))))
 
 (defn parity [a-seq]
-  (let [aux (fn [acc s]
-              (if (empty? s)
-                acc
-                (let [x (first s)]
-                  (recur ((if (contains? acc x) disj conj) acc x)
-                         (rest s)))))]
-    (aux #{} a-seq)))
+  (loop [acc #{} s a-seq]
+    (if (empty? s)
+      acc
+      (let [x (first s)]
+        (recur ((if (contains? acc x) disj conj) acc x)
+               (rest s))))))
 
 (defn fast-fibo [n]
-  (let [aux (fn [f0 f1 n]
-              (cond (= 0 n) f0
-                    (= 1 n) f1
-                    :else (recur f1 (+ f0 f1) (dec n))))]
-    (aux 0 1 n)))
+  (loop [f0 0 f1 1 n n]
+    (cond (= 0 n) f0
+          (= 1 n) f1
+          :else (recur f1 (+ f0 f1) (dec n)))))
 
 (defn cut-at-repetition [a-seq]
   (loop [set #{}
